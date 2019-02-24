e8af21f55fd2f9860958bf5241c0dc39d479c3ec
mahhov <mahhov1@gmail.com>
2019-02-24 12:28:30 -0800

    use fs.promises to read html file in file http server

--- src/server/HtmlHttpServer.js
+++ src/server/HtmlHttpServer.js
@@ -1,19 +1,10 @@
 const http = require('http');
-const fs = require('fs'); // todo const fs = require('fs').promises;
+const fs = require('fs').promises;
 const path = require('path');
-const PromiseX = require('../PromiseX');
 
 class FileHttpServer {
 	constructor(relPath, port) {
-		this.readFile = new PromiseX();
-
-		fs.readFile(path.resolve(__dirname, relPath), (err, read) => {
-			if (err)
-				this.readFile.reject(err);
-			else
-				this.readFile.resolve(read);
-		});
-
+		this.readFile = fs.readFile(path.resolve(__dirname, relPath));
 		this.port = port;
 	}
 
