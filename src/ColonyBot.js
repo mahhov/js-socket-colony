b96676cf4f3695d04dfab7876e02dcb4da1694f9
mahhov <mahhov1@gmail.com>
2019-03-01 15:58:34 -0800

    try out a tradational for loop more efficient verion of vulnurability bot calc

--- src/ColonyBot.js
+++ src/ColonyBot.js
@@ -29,11 +29,26 @@ class ColonyBot {
 	}
 
 	static score(board, tile) {
-		let counts = [0, 0, 0];
+		let score = [0, 0, 0];
+		const VULNERABILITY_WEIGHTS = [0, 0, 0, 0, 5 * 5 / 8, 6 * 6 / 8, 12, 7 * 7 / 8, 8];
+
 		for (let x = 0; x < board.width; x++)
 			for (let y = 0; y < board.height; y++)
-				counts[board.tiles[x][y]]++;
-		return counts[tile] - counts[3 - tile];
+				if (!board.tiles[x][y])
+					for (let tile = 1; tile < 3; tile++) {
+						let vulnerability = board.getNearbyInBoundsOfTile(x, y, 1, tile).length;
+						score[tile] -= VULNERABILITY_WEIGHTS[vulnerability];
+					}
+				else
+					score[board.tiles[x][y]]++;
+
+		return score[tile] - score[3 - tile];
+
+		// let counts = [0, 0, 0];
+		// for (let x = 0; x < board.width; x++)
+		// 	for (let y = 0; y < board.height; y++)
+		// 		counts[board.tiles[x][y]]++;
+		// return counts[tile] - counts[3 - tile];
 	}
 }
 
