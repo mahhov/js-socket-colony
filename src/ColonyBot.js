b879867eb811e64661c64163d6b836a451e1651c
mahhov <mahhov1@gmail.com>
2019-03-01 16:33:36 -0800

    bot ai, include multiple scoring functions, thye're all bad

--- src/ColonyBot.js
+++ src/ColonyBot.js
@@ -29,6 +29,20 @@ class ColonyBot {
 	}
 
 	static score(board, tile) {
+		return ColonyBot.scoreVulnerability(board, tile);
+	}
+
+	// simply counts tiles per player
+	static scoreCounts(board, tile) {
+		let counts = [0, 0, 0];
+		for (let x = 0; x < board.width; x++)
+			for (let y = 0; y < board.height; y++)
+				counts[board.tiles[x][y]]++;
+		return counts[tile] - counts[3 - tile];
+	}
+
+	// penalizes for empty tiles of big-loss, i.e., opponent makes a move that causes tile to lose 6 count
+	static scoreVulnerability(board, tile) {
 		let score = [0, 0, 0];
 		const VULNERABILITY_WEIGHTS = [0, 0, 0, 0, 5 * 5 / 8, 6 * 6 / 8, 12, 7 * 7 / 8, 8];
 
@@ -43,12 +57,38 @@ class ColonyBot {
 					score[board.tiles[x][y]]++;
 
 		return score[tile] - score[3 - tile];
+	}
 
-		// let counts = [0, 0, 0];
-		// for (let x = 0; x < board.width; x++)
-		// 	for (let y = 0; y < board.height; y++)
-		// 		counts[board.tiles[x][y]]++;
-		// return counts[tile] - counts[3 - tile];
+	// penalizes tiles surrounded by empty tiles
+	static scorePointsOfAttack(board, tile) {
+		let score = [0, 0, 0];
+
+		for (let x = 0; x < board.width; x++)
+			for (let y = 0; y < board.height; y++)
+				if (board.tiles[x][y]) {
+					let pointsOfAttack = board.getNearbyInBoundsOfTile(x, y, 1, 0);
+					score[board.tiles[x][y]] += 1 / (pointsOfAttack.length + 1);
+				}
+
+		return score[tile] - score[3 - tile];
+	}
+
+	// penalizes tiles part of a potential big loss; i.e., if the opponent can make a move that takes 6 points, those 6 points are penalized
+	static scoreRisk(board, tile) {
+		let score = [0, 0, 0];
+
+		for (let x = 0; x < board.width; x++)
+			for (let y = 0; y < board.height; y++) {
+				let t = board.tiles[x][y];
+				if (t) {
+					let losses = board.getNearbyInBoundsOfTile(x, y, 1, 0)
+						.map(({x, y}) => board.getNearbyInBoundsOfTile(x, y, 1, t).length);
+					let maxLoss = Math.max(...losses);
+					score[t] += 1 / (maxLoss + 5);
+				}
+			}
+
+		return score[tile] - score[3 - tile];
 	}
 }
 
